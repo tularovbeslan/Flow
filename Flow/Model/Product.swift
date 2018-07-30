768ef3b82c6375216971b1b41eef25e21b16920f
Beslan Tularov <tularovbeslan@gmail.com>
2018-07-31 02:44:22 +0300

    models created

---
+++ Flow/Model/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  Flow
+//
+//  Created by workmachine on 31.07.2018.
+//  Copyright © 2018 Beslan Tularov Ramazanovich. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+	let id: Int
+	let name: String
+}
---
+++ Flow/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Flow
+//
+//  Created by workmachine on 31.07.2018.
+//  Copyright © 2018 Beslan Tularov Ramazanovich. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+	let id: Int
+	let name: String
+	let token: String
+}
